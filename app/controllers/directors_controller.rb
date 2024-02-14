fd66c2cc32f7b1a5b5ca26d46eb982b7ccf85888
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-02-14 22:05:57 +0000

    msmgui

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,19 @@
 class DirectorsController < ApplicationController
+ 
+ def create
+  d = Director.new
+  
+  d.name = params.fetch("direc_name")
+  d.dob = params.fetch("direc_dob")
+  d.bio = params.fetch("direc_bio")
+  d.image = params.fetch("direc_image")
+
+  d.save
+
+ redirect_to("/directors")
+ end
+  
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
@@ -35,5 +50,39 @@ class DirectorsController < ApplicationController
     @eldest = directors_by_dob_asc.at(0)
 
     render({ :template => "director_templates/eldest" })
+  end
+
+
+  def destroy
+    the_id = params.fetch("an_id")
+    
+    matching_records = Director.where ({:id => the_id})
+    
+    the_director = matching_records.at(0)
+    
+    the_director.destroy
+    
+    redirect_to("/directors")
+  
+  end
+
+
+   def update 
+    a_id = params.fetch("the_id")
+
+    matching_records= Director.where({ :id => a_id})
+    
+    the_direc = matching_records.at(0)
+    
+    
+       the_direc.name = params.fetch("direc_name")
+       the_direc.dob = params.fetch("direc_dob")
+       the_direc.bio = params.fetch("direc_bio")
+       the_direc.image = params.fetch("direc_image")
+      
+       the_direc.save
+        
+    
+    redirect_to("/directors/#{the_direc.id}")
   end
 end
